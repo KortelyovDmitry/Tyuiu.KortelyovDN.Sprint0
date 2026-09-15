733b7dbd78265f4da9be3ab9e2b619bfd606ee04
Дмитрий Кортелёв <Tyuiu.KortelyovDN@outlook.com>
2026-09-15 20:25:58 +0300

    Task6

---
+++ Tyuiu.KortelyovDN.Sprint0.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using Mono.Cecil.Cil;
+using Tyuiu.KortelyovDN.Sprint0.Task6.V0.Lib;
+namespace Tyuiu.KirshinKO.Sprint0.Task6.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.AdditionArray(numbers);
+            Assert.AreEqual(15, res);
+        }
+
+        [TestMethod]
+        public void CheckSubtractionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.SubtractionArray(numbers);
+            Assert.AreEqual(-15, res);
+        }
+        [TestMethod]
+        public void CheckMultArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.MultiplicationArray(numbers);
+            Assert.AreEqual(120, res);
+        }
+    }
+}
